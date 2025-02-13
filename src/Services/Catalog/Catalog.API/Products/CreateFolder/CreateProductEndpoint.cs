a80834e0955453a9c89b188580339857ac6bc35a
Asif.Khan <asifkhan88012@gmail.com>
2025-02-13 11:09:38 +0530

    add product  model class, for cqrs added create product (handler and endpoint) files, and mediatr nuget in catalog api

---
+++ src/Services/Catalog/Catalog.API/Products/CreateFolder/CreateProductEndpoint.cs
@@ -0,0 +1,6 @@
+﻿namespace Catalog.API.Products.CreateFolder
+{
+    public class CreateProductEndpoint
+    {
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateFolder/CreateProductHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateFolder
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        :IRequest<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+
+    internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            //business logic to create a product
+            throw new NotImplementedException();
+        }
+    }
+}
