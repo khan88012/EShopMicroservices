ddaf69f200ad11d9c7dbbc44427bd68a70039db5
Asif.Khan <asifkhan88012@gmail.com>
2025-02-14 12:20:49 +0530

    Mapster installed and ICarterModule used in the endpoint

--- src/Services/Catalog/Catalog.API/Products/CreateFolder/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateFolder/CreateProductEndpoint.cs
@@ -1,8 +1,15 @@
-﻿namespace Catalog.API.Products.CreateFolder
+﻿using Carter;
+
+namespace Catalog.API.Products.CreateFolder
 {
     public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
     public record CreateProductResponse(Guid Id);
-    public class CreateProductEndpoint
+    public class CreateProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            //we need a mapper to change request (carter) object to command(mediatr) object , thats why install nuget package Mapster in building blocks
+            throw new NotImplementedException();
+        }
     }
 }
