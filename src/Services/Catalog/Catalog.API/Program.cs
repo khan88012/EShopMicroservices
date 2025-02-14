5a39504cc9e142fe09583540c3ca4f8be5fdfa23
Asif.Khan <asifkhan88012@gmail.com>
2025-02-14 12:56:34 +0530

    injected and registerd carter and mediatr services

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,8 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 var app = builder.Build();
 
 //configure the htttp request pipeline
+app.MapCarter();
 
 app.Run();
