4ea08a58a2bdf005b2f0a3da84f3d7a00125a24b
Asif.Khan <asifkhan88012@gmail.com>
2025-02-14 12:50:04 +0530

    add post method for the endpoint" create product" also added global using file to keep all the using statements at one place.

---
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -0,0 +1,3 @@
+﻿global using MediatR;
+global using Mapster;
+global using Carter;
--- src/Services/Catalog/Catalog.API/Products/CreateFolder/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateFolder/CreateProductEndpoint.cs
@@ -1,4 +1,4 @@
-﻿using Carter;
+﻿//removed name spaces as added globalUsing file
 
 namespace Catalog.API.Products.CreateFolder
 {
@@ -9,7 +9,21 @@ namespace Catalog.API.Products.CreateFolder
         public void AddRoutes(IEndpointRouteBuilder app)
         {
             //we need a mapper to change request (carter) object to command(mediatr) object , thats why install nuget package Mapster in building blocks
-            throw new NotImplementedException();
+            app.MapPost("/products",
+                async (CreateProductRequest request, ISender sender) =>
+                {
+                    //Adapt(mapping method) wasnt being recognised here so I downgraded Mapster version to 7.0.0
+                    
+                    var command = request.Adapt<CreateProductCommand>();
+                    var result = await sender.Send(command);
+                    var response = result.Adapt<CreateProductResponse>();
+                    return Results.Created($"/products/{response.Id}", response);
+                })
+                .WithName("CreateProduct")
+                .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Create Product")
+                .WithDescription("Create Product");
         }
     }
 }
