e7f856fc0eddf10cb29a7d7b829e5c3fed47bc7e
Asif.Khan <asifkhan88012@gmail.com>
2025-02-18 11:16:17 +0530

    created validator class, used validation for the create product and registered validation dependency. ran the project fired post enpoint with name as empty and it threw exception

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,4 +1,5 @@
 ﻿
+
 namespace Catalog.API.Products.CreateFolder
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
@@ -6,11 +7,35 @@ namespace Catalog.API.Products.CreateFolder
 
     public record CreateProductResult(Guid Id);
 
-    //The reason they are using the constructor "CreateProductHandler(IDocumentSession session)" directly in the class name is because of Primary Constructors, a feature introduced in C# 12.
-    internal class CreateProductHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price is required");
+
+        }
+    }
+
+        //The reason they are using the constructor "CreateProductHandler(IDocumentSession session)" directly in the class name is because of Primary Constructors, a feature introduced in C# 12.
+        internal class CreateProductHandler
+            (IDocumentSession session, IValidator<CreateProductCommand> validator) 
+            : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
+                //writng validation logic
+
+                var result = await validator.ValidateAsync(command, cancellationToken);
+                var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+                if(errors.Any())
+                {
+                    throw new ValidationException(errors.FirstOrDefault());
+                }
+
+
             //business logic to create a product
 
             var product = new Product
@@ -25,6 +50,8 @@ namespace Catalog.API.Products.CreateFolder
             //todo save to database
             session.Store(product);
             await session.SaveChangesAsync(cancellationToken);
+
+                //return CreateProductResult result
             return new CreateProductResult(product.Id);
         }
     }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
+//injecting validator service
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
