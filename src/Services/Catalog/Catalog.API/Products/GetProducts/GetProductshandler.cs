50d176dba241be4bb1af01225c323118324ddef0
Asif.Khan <asifkhan88012@gmail.com>
2025-02-26 13:50:28 +0530

    added martens  pagination. verified the results by sending postman requests it works "https://localhost:5050/products?pageNumber=3&pageSize=5"

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductshandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductshandler.cs
@@ -1,7 +1,8 @@
 ﻿
+
 namespace Catalog.API.Products.GetProducts
 {
-    public record GetProductQuery() : IQuery<GetProductsResult>;
+    public record GetProductQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
     public record GetProductsResult(IEnumerable<Product> Products);
     internal class GetProductsQueryhandler
         (IDocumentSession session)
@@ -9,7 +10,7 @@ namespace Catalog.API.Products.GetProducts
     {
         public async Task<GetProductsResult> Handle(GetProductQuery query, CancellationToken cancellationToken)
         {
-                var products = await session.Query<Product>().ToListAsync(cancellationToken);
+                var products = await session.Query<Product>().ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
             return new GetProductsResult(products);
         }
     }
